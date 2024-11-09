d9d037b6a8c026e6bce641aa536a8bb8a0a10fdb
Nathan Foo <ntfoo27@outlook.com>
2024-11-09 01:07:38 -0600

    Add navbar routes and fonts

--- client-next/components/sections/navbar.tsx
+++ client-next/components/sections/navbar.tsx
@@ -1,7 +1,15 @@
+import Link from "next/link";
+
 const Navbar = () => {
     return (
-        <div>
-            Navbar
+        <div className="font-[CutiePatootie] text-[1.3rem] flex items-center justify-between top-0 left-0 w-[100%] fixed py-6 px-12">
+            <Link href="/">nathan foo</Link>
+
+            <div className="navbar">
+                <Link href="/" className="relative ml-10">home</Link>
+                <Link href="/about" className="relative ml-10">about</Link>
+                <Link href="/projects" className="relative ml-10">projects</Link>
+            </div>
         </div>
     );
 }
