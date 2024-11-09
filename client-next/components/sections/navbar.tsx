2ca110ccf09d7f6155b480ca21189d327597f601
Nathan Foo <ntfoo27@outlook.com>
2024-11-09 02:33:19 -0600

    Update navbar, work route

--- client-next/components/sections/navbar.tsx
+++ client-next/components/sections/navbar.tsx
@@ -3,12 +3,12 @@ import Link from "next/link";
 const Navbar = () => {
     return (
         <div className="font-[CutiePatootie] absolute text-[1.3rem] flex items-center justify-between top-0 left-0 w-[100%] py-6 px-12">
-            <Link href="/">nathan foo</Link>
+            <Link href="/" className="font-extrabold">✰ nathan foo</Link>
 
             <div className="navbar">
-                <Link href="/" className="relative ml-10">home</Link>
                 <Link href="/about" className="relative ml-10">about</Link>
-                <Link href="/projects" className="relative ml-10">projects</Link>
+                <Link href="/work" className="relative ml-10">work</Link>
+                <Link href="/contact" className="relative ml-10">contact</Link>
             </div>
         </div>
     );
